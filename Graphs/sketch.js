07f4502960a50453cd5794a84cdd9dbacae6639e
diegoserodio <diegocosta2113@gmail.com>
2019-07-13 12:47:30 -0300

    Trying to send a CSV file to p5.js sketch

--- Graphs/sketch.js
+++ Graphs/sketch.js
@@ -1,7 +1,14 @@
+var methods={
+	csv_file: "Test"
+};
+
 function setup() {
 	createCanvas(windowWidth, windowHeight);
+	console.log(methods.csv_file);
 }
 
 function draw() {
 	background(0, 0, 0);
 }
+
+exports.data=methods;
